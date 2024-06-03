9153077fdcacd912fc9c5bf818babdf850fed8b4
肖懿 <2231597633@qq.com>
2024-06-03 23:02:31 +0800

    线程池和CountDownLatch使用

---
+++ trip-modules/trip-article-server/src/main/java/com/pzhu/article/config/AppConfig.java
@@ -0,0 +1,24 @@
+package com.pzhu.article.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.*;
+
+@Configuration
+public class AppConfig {
+
+    // 线程池配置
+    @Bean
+    public ThreadPoolExecutor businessThreadPoolExecutor() {
+
+        // 创建线程池的方式
+        // 1. Executors 创建 =》 不推荐
+        // 不推荐原因是因为默认创建的工作队列，使用的是 LinkedBlockingQueue 队列，且默认容量为 Integer 的最大值
+        // 工作队列的容量过大，会导致核心线程工作过载，队列中任务数过多，且非核心线程无法参与处理，最终导致内存溢出
+        //ExecutorService service = Executors.newFixedThreadPool(50);// 最多50个线程
+        // 2. 直接 new ThreadPoolExecutor 对象 =》 推荐使用
+        return new ThreadPoolExecutor(10, 50, 10, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(100));
+    }
+}
--- trip-modules/trip-article-server/src/main/java/com/pzhu/article/service/impl/DestinationServiceImpl.java
+++ trip-modules/trip-article-server/src/main/java/com/pzhu/article/service/impl/DestinationServiceImpl.java
@@ -9,20 +9,27 @@ import com.pzhu.article.mapper.DestinationMapper;
 import com.pzhu.article.service.DestinationService;
 import com.pzhu.article.service.RegionService;
 import com.pzhu.article.qo.DestinationQuery;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Service
 public class DestinationServiceImpl extends ServiceImpl<DestinationMapper, Destination> implements DestinationService {
 
     private final RegionService regionService;
+    private final ThreadPoolExecutor businessThreadPoolExecutor;
 
-    public DestinationServiceImpl(RegionService regionService) {
+
+    public DestinationServiceImpl(RegionService regionService, ThreadPoolExecutor businessThreadPoolExecutor) {
         this.regionService = regionService;
+        this.businessThreadPoolExecutor = businessThreadPoolExecutor;
     }
 
     /**
@@ -110,13 +117,31 @@ public class DestinationServiceImpl extends ServiceImpl<DestinationMapper, Desti
         return destinations;
         /*
         // 查询所有目的地的下一级目的地
+        // 如何等待所有异步线程都执行完成后，主线程再继续执行
+        // 1. 创建一个倒计时对象 CountDownLatch 设置倒计时的值为目的地的数量
+        CountDownLatch latch = new CountDownLatch(destinations.size());
         for (Destination destination : destinations) {
-            // 清除之前的条件
-            wrapper.clear();
-            // 限制只查 10 条， lastSql 指在 sql 后面加上这一句
-            List<Destination> children = list(wrapper.eq("parent_id", destination.getId()).last("limit 10"));
-            destination.setChildren(children);
+            // 运用线程提高sql查询效率，但一般不这样做，因为循环产生的sql太多，查询效率会变低
+            // 采用未注释的方法，运用动态sql，进行连表查询
+            businessThreadPoolExecutor.execute(() -> {// 异步
+                // 清除之前的条件
+                wrapper.clear();
+                // 限制只查 10 条， lastSql 指在 sql 后面加上这一句
+                List<Destination> children = list(wrapper.eq("parent_id", destination.getId()).last("limit 10"));
+                destination.setChildren(children);
+
+                // 2. 倒计时数量-1
+                latch.countDown();
+            });
         }
+
+        try {
+            // 3， 最终返回结果前，阻塞等待所有任务完成
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         return destinations;*/
     }
 }
