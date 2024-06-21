7f9e3cbb770ed4a3d129e06de2ceb56702334e6d
肖懿 <2231597633@qq.com>
2024-06-21 22:17:49 +0800

    更新攻略对象

--- trip-modules/trip-article-server/src/main/java/com/pzhu/article/service/impl/StrategyServiceImpl.java
+++ trip-modules/trip-article-server/src/main/java/com/pzhu/article/service/impl/StrategyServiceImpl.java
@@ -13,7 +13,9 @@ import com.pzhu.article.utils.OssUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -33,13 +35,54 @@ public class StrategyServiceImpl extends ServiceImpl<StrategyMapper, Strategy> i
         this.strategyContentMapper = strategyContentMapper;
     }
 
+    /**
+     * 查询攻略对象，设置攻略内容
+     * @param id
+     * @return
+     */
+    @Override
+    public Strategy getById(Serializable id) {
+        Strategy strategy = super.getById(id);
+        StrategyContent content = strategyContentMapper.selectById(id);
+        strategy.setContent(content);
+        return strategy;
+    }
+
+    /**
+     * 保存攻略对象
+     * @param entity
+     * @return
+     */
     @Transactional(rollbackFor = Exception.class)
     @Override
     public boolean save(Strategy entity) {
+        return doSaveOrUpdate(entity);
+    }
+
+    /**
+     * 更新攻略对象
+     * @param entity
+     * @return
+     */
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public boolean updateById(Strategy entity) {
+        return doSaveOrUpdate(entity);
+    }
+
+    /**
+     * 保存或者更新操作
+     * @param entity
+     * @return
+     */
+    private boolean doSaveOrUpdate(Strategy entity){
         // 1. 完成封面图片的上传，且得到 url 后重新设置到 cover 中
-        String fileName = UUID.randomUUID().toString();
-        String url = OssUtil.uploadImgByBase64("images/strategies", fileName + ".jpg", entity.getCoverUrl());
-        entity.setCoverUrl(url);
+        if (!StringUtils.isEmpty(entity.getCoverUrl()) || entity.getCoverUrl().startsWith("http")){
+            String fileName = UUID.randomUUID().toString();
+            String url = OssUtil.uploadImgByBase64("images/strategies", fileName + ".jpg", entity.getCoverUrl());
+            entity.setCoverUrl(url);
+        }
+
         // 2. 补充分类名称
         StrategyCatalog catalog = strategyCatalogService.getById(entity.getCatalogId());
         entity.setCatalogName(catalog.getName());
@@ -57,21 +100,32 @@ public class StrategyServiceImpl extends ServiceImpl<StrategyMapper, Strategy> i
         // 4. 查询主题，设置主题名称
         StrategyTheme theme = strategyThemeService.getById(entity.getThemeId());
         entity.setThemeName(theme.getName());
-        // 5. 设置创建时间
-        entity.setCreateTime(new Date());
-        // 6. 设置各种数量为0\
-        entity.setViewnum(0);
-        entity.setSharenum(0);
-        entity.setThumbsupnum(0);
-        entity.setFavornum(0);
-        entity.setReplynum(0);
-        // 7. 重新设置默认状态，覆盖前端提交的值
-        entity.setState(Strategy.STATE_NORMAL);
-        // 8. 保存攻略对象，得到攻略自增的 id
-        boolean save = super.save(entity);
-        // 9. 将攻略 id 设置到攻略内容对象中，并保存内容对象
+
+        // 判断是更新还是新增
+        if (entity.getId() == null){
+            // 5. 设置创建时间
+            entity.setCreateTime(new Date());
+            // 6. 设置各种数量为0\
+            entity.setViewnum(0);
+            entity.setSharenum(0);
+            entity.setThumbsupnum(0);
+            entity.setFavornum(0);
+            entity.setReplynum(0);
+            // 7. 重新设置默认状态，覆盖前端提交的值
+            entity.setState(Strategy.STATE_NORMAL);
+            // 8. 保存攻略对象，得到攻略自增的 id
+            boolean save = super.save(entity);
+            // 9. 将攻略 id 设置到攻略内容对象中，并保存内容对象
+            StrategyContent content = entity.getContent();
+            content.setId(entity.getId());
+            return save && strategyContentMapper.insert(content) > 0;
+        }
+
+        // 更新操作
+        boolean ret = super.updateById(entity);
         StrategyContent content = entity.getContent();
         content.setId(entity.getId());
-        return save && strategyContentMapper.insert(content) > 0;
+        int row = strategyContentMapper.updateById(entity.getContent());
+        return ret && row > 0;
     }
 }
