06343caa44a4aa9f391648accde1d9f1019a9616
2231597633@qq.com <2231597633@qq.com>
2024-07-06 21:34:07 +0800

    评论模块搭建

---
+++ trip-modules/trip-comment-server/src/main/java/com/pzhu/comment/TripCommentApplication.java
@@ -0,0 +1,11 @@
+package com.pzhu.comment;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripCommentApplication {
+
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(TripCommentApplication.class, args);
+    }
+}
