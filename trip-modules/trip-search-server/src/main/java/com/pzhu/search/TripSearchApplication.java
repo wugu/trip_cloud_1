6365f70612d86bb11b43dfb5411c61c0d1c74dc5
2231597633@qq.com <2231597633@qq.com>
2024-07-10 14:52:43 +0800

    搜索服务搭建

---
+++ trip-modules/trip-search-server/src/main/java/com/pzhu/search/TripSearchApplication.java
@@ -0,0 +1,10 @@
+package com.pzhu.search;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripSearchApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(TripSearchApplication.class, args);
+    }
+}
